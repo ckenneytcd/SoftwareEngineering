a4e5d56ac8e8bebdc19bb70f9dd33247a7f425bc
ckenneytcd <78963834+ckenneytcd@users.noreply.github.com>
2021-10-12 19:38:14 +0100

    DNE

--- LCAjava/LCATests.java
+++ LCAjava/LCATests.java
@@ -16,13 +16,16 @@ public class LCATests {
 		tree.root.right.left = new Node(6);
 		tree.root.right.right = new Node(7);
 		
+		assertEquals("Does not exisit: LCA of (0, 5)", -1, tree.findLCA(0, 5));
 		assertEquals("LCA of (4, 5)", 2, tree.findLCA(4, 5));
 		assertEquals("LCA of (4, 6)", 1, tree.findLCA(4, 6));
 		assertEquals("LCA of (3, 4)", 1, tree.findLCA(3, 4));
 		assertEquals("LCA of (2, 4)", 2, tree.findLCA(2, 4));
+		assertEquals("LCA of (2, 3)", 1, tree.findLCA(2, 3));
     		
 
     }
+
     
 }
 
